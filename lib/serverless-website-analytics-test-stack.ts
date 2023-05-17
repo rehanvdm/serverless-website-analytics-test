1d1b8a5d59ce713b18e6d925a8219481a11a5c11
rehanvandermerwe <rehan.merwe@messagebird.com>
2023-05-17 06:01:59 +0200

    Init

--- lib/serverless-website-analytics-test-stack.ts
+++ lib/serverless-website-analytics-test-stack.ts
@@ -6,11 +6,6 @@ export class ServerlessWebsiteAnalyticsTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessWebsiteAnalyticsTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
